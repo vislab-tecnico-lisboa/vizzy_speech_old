c09ea0128fedff343719f9d024c81541ae21dfaf
João Avelino <joao.manuel.avelino@gmail.com>
2017-07-11 02:03:50 +0100

    Fully functional Vizzy of Oz. Needs testing.
    Fully functional Vizzy of Oz.
    Needs testing.

--- woz_dialog/ros_woz_dialog_project/web_tools/js/button_events.js
+++ woz_dialog/ros_woz_dialog_project/web_tools/js/button_events.js
@@ -1,6 +1,102 @@
 var expanded = false;
 var expandedButtonId = 0;
 
+
+/******************ROS STUFF ***********************************************/
+/*  var ros = new ROSLIB.Ros({
+    url : 'ws://localhost:9090'
+  });
+
+    var speechClient = new ROSLIB.ActionClient({
+    ros : ros,
+    serverName : '/woz_dialog/speaker',
+    actionName : 'woz_dialog_msgs/SpeechAction'
+  });
+
+    var goal = new ROSLIB.Goal({
+    actionClient : speechClient,
+    goalMessage : {
+      language : 'por-PRT',
+      voice: 'Joana',
+      message: 'Mensagem vazia'
+    }
+  });
+
+  goal.on('feedback', function(feedback) {
+    console.log('Feedback: ' + feedback.status);
+  });
+
+  goal.on('result', function(result) {
+    console.log('Final Result: ' + result.success);
+  });
+
+  ros.on('connection', function() {
+    console.log('Connected to websocket server.');
+  });
+
+  ros.on('error', function(error) {
+    console.log('Error connecting to websocket server: ', error);
+  });
+
+  ros.on('close', function() {
+    console.log('Connection to websocket server closed.');
+  });*/
+
+
+/***************************************************************************/
+
+function speakYesNo(button)
+{
+
+	var index = Math.floor(Math.random() * (3 - 0 + 1)) + 0;
+	var toSay = {};
+
+	if(button.id == "btn_yes")
+	{
+		toSay = dictionary["btn_yes"].valor[index];
+	}
+	else
+	{
+		toSay = dictionary["btn_no"].valor[index];
+	}
+
+	alert(toSay);
+	//goal.goalMessage.message = toSay;
+
+
+}
+
+function speak(clickedJoint)
+{
+	
+	//Speak
+	//Select a random sentence
+	var index = Math.floor(Math.random() * (3 - 0 + 1)) + 0;
+
+	//Get the sentence of that joint button
+	var toSay = dictionary[clickedJoint.id].valor[index];
+	alert(toSay);
+	//goal.goalMessage.message = toSay;
+
+
+	
+	//Vizzy spoke. Unexpand everything
+	id = expandedButtonId;
+	var query = "g:not(#"+id+").group_button"
+	var elementsToUnBlur = document.querySelectorAll(query);
+
+	for(var i=0; i<elementsToUnBlur.length; i++)
+	{
+		var button = elementsToUnBlur[i];
+	  	button.classList.remove('chilled')
+	}
+
+	unexpand_inner(id);
+
+	expanded = false;
+	expandedButtonId = 0;
+}
+
 function expand(clickedButton)
 {
   
@@ -61,7 +157,6 @@ function expand_inner(id)
 	for(var i=0; i < elementsToSee.length; i++)
 	{
 		var joint = elementsToSee[i];
-		console.log(joint.id);
 		joint.style.display = "inline";
 	}
 
@@ -76,7 +171,6 @@ function unexpand_inner(id)
 	for(var i=0; i < elementsToSee.length; i++)
 	{
 		var joint = elementsToSee[i];
-		console.log(joint.id);
 		joint.style.display = "none";
 	}
 
